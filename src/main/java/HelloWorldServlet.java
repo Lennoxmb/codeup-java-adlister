8a51a60007024e07ee28859a0a176063cd197568
Lennox Ballard <noellemb@gmail.com>
2023-05-09 10:43:53 -0500

    jsp and jstl walkthrough

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,20 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        String name = req.getParameter("name");
+        if (name != null){
+            res.getWriter().println("<h1>Hello, " + name + "!</h1>");
+        } else {
+            res.getWriter().println("<h1>Hello, World!</p>");
+        }
     }
 }
