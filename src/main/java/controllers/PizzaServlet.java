74628461eb6ad74e6562221faf119b47174004c9
Lennox Ballard <noellemb@gmail.com>
2023-05-10 10:17:38 -0500

    pizza exercise

---
+++ src/main/java/controllers/PizzaServlet.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PizzaServlet extends HttpServlet{
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getRequestDispatcher("/blog/createPost.jsp").forward(req, resp);
+        }
+
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String post = req.getParameter("post");
+            String title = req.getParameter("title");
+            String[] topics = req.getParameterValues("topic");
+            Post submittedPost = new Post(title, post, topics);
+            String fontSizeRequested = req.getParameter("font-size");
+            System.out.println(fontSizeRequested);
+            req.setAttribute("post", submittedPost);
+            req.setAttribute("font", fontSizeRequested);
+            req.getRequestDispatcher("/blog/createPost.jsp").forward(req, resp);
+        }
+
+}
