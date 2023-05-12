4df853f35dadcc55fdc8a0027a01c18cacc7377a
Lennox Ballard <noellemb@gmail.com>
2023-05-12 15:02:14 -0500

    session exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
         //the profile page is displayed if they're already logged in and try to go to login
                 if (request.getSession().getAttribute("username") != null) {
             response.sendRedirect("/profile");
