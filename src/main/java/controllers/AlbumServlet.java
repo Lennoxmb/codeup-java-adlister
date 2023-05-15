8fe0a51edf611f762643b9e8bba5d5142d164a41
Lennox Ballard <noellemb@gmail.com>
2023-05-15 11:34:20 -0500

    jdbc lecture

--- src/main/java/controllers/AlbumServlet.java
+++ src/main/java/controllers/AlbumServlet.java
@@ -5,10 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-    @WebServlet(name = "controllers.AlbumServlet", urlPatterns = "/album")
+    @WebServlet("/album")
 public class AlbumServlet extends HttpServlet {
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            Album albumDao = new Album();
+            List<Album>
         }
     }
 }
