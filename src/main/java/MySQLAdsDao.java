7da77f7fa7d3119964c79f782bce61bc39ad9e98
Lennox Ballard <noellemb@gmail.com>
2023-05-16 10:05:30 -0500

    jdbc exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,3 +1,26 @@
-public class MySQLAdsDao implements AdsDao{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        //class should have a private instance property named connection of type Connection that is initialized in the constructor. Define your constructor so that it accepts an instance of your Config class so that it can obtain the database credential
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://" + config.getUrl() + "/" + config.getDatabaseName(),
+                config.getUser(),
+                config.getPassword()
+        );
+    }
 
 }
